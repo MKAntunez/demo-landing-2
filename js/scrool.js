fd77f2f8331042de624cbc78ae6e897cac3777e5
MKAntunez <antunezmiguel365@gmail.com>
2022-01-13 20:43:26 -0500

    primer ajuste

---
+++ js/scrool.js
@@ -0,0 +1,5 @@
+window.addEventListener('scroll', function(){
+	let nav = document.querySelector('div');
+	let windowPosition = window.scrollY > 0;
+	nav.classList.toggle('sticky', windowPosition);
+})

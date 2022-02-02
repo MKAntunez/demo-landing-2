ffb4c30303b132bc94798937931f413bad3fa370
MKAntunez <antunezmiguel356@gmail.com>
2022-02-02 16:12:09 -0500

    ajustes del miercoles

---
+++ js/app.js
@@ -0,0 +1,29 @@
+(function () {
+    "use strict";
+  
+    var carousels = function () {
+      $(".owl-carousel1").owlCarousel({
+        loop: true,
+        center: true,
+        margin: 0,
+        responsiveClass: true,
+        nav: false,
+        responsive: {
+          0: {
+            items: 1,
+            nav: false
+          },
+         
+          766: {
+            items: 3,
+            nav: true
+          }
+        }
+      });
+    };
+  
+    (function ($) {
+      carousels();
+    })(jQuery);
+  })();
+  
--- js/scrool.js
+++ js/scrool.js
@@ -4,11 +4,34 @@ window.addEventListener('scroll', function(){
 	nav.classList.toggle('sticky', windowPosition);
 })
 
+window.addEventListener('load', function(){
+	new Glider(document.querySelector('.carousel__lista'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		draggable: true,
+		
+		
+		responsive: [
+			{
+			  // screens greater than >= 775px
+			  breakpoint: 500,
+			  settings: {
+				// Set to `auto` and provide item width to adjust to viewport
+				slidesToShow: 2,
+				slidesToScroll: 2
+			  }
+			},{
+			  // screens greater than >= 1024px
+			  breakpoint: 992,
+			  settings: {
+				slidesToShow: 4,
+				slidesToScroll: 4
+			  }
+			}
+		]
+	});
+});
 
-function cambiarClase(){
-	document.getElementById("headingOne").classList.toggle("btn btn-link collapsed");
-}
-document.getElementById("boton").onclick = funtion();{
-	canbiarClase();
-}
 
+
+  
